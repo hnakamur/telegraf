f77768bda6ce6ed9742cd9c2c765e97e0a41ae2f
Hiroaki Nakamura <hnakamur@gmail.com>
2016-03-05 11:34:04 +0900

    Add LTSVParser to the parsers registry

--- plugins/parsers/registry.go
+++ plugins/parsers/registry.go
@@ -8,6 +8,7 @@ import (
 	"github.com/influxdata/telegraf/plugins/parsers/graphite"
 	"github.com/influxdata/telegraf/plugins/parsers/influx"
 	"github.com/influxdata/telegraf/plugins/parsers/json"
+	"github.com/influxdata/telegraf/plugins/parsers/ltsv"
 )
 
 // ParserInput is an interface for input plugins that are able to parse
@@ -22,6 +23,9 @@ type Parser interface {
 	// Parse takes a byte buffer separated by newlines
 	// ie, `cpu.usage.idle 90\ncpu.usage.busy 10`
 	// and parses it into telegraf metrics
+	//
+	// NOTE: For the LTSV parser, you need to call an additional `Parse(nil)`
+	// if the last data does not end with the newline `\n`.
 	Parse(buf []byte) ([]telegraf.Metric, error)
 
 	// ParseLine takes a single string metric
@@ -38,7 +42,7 @@ type Parser interface {
 // Config is a struct that covers the data types needed for all parser types,
 // and can be used to instantiate _any_ of the parsers.
 type Config struct {
-	// Dataformat can be one of: json, influx, graphite
+	// Dataformat can be one of: json, influx, graphite, ltsv
 	DataFormat string
 
 	// Separator only applied to Graphite data.
@@ -48,8 +52,34 @@ type Config struct {
 
 	// TagKeys only apply to JSON data
 	TagKeys []string
-	// MetricName only applies to JSON data. This will be the name of the measurement.
+	// MetricName only applies to JSON data and LTSV data. This will be the name of the measurement.
 	MetricName string
+
+	// TimeLabel only applies to LTSV data. This will be the label of the timestamp.
+	// If this label is not found in the measurement, the current time will be used.
+	TimeLabel string
+	// TimeFormat only applies to LTSV data. This will be the format of the timestamp.
+	// Please see https://golang.org/pkg/time/#Parse for the format string.
+	TimeFormat string
+	// StrFieldLabels only applies to LTSV data. This will be the labels of string fields.
+	StrFieldLabels []string
+	// IntFieldLabels only applies to LTSV data. This will be the labels of integer fields.
+	IntFieldLabels []string
+	// FloatFieldLabels only applies to LTSV data. This will be the labels of float fields.
+	FloatFieldLabels []string
+	// BoolFieldLabels only applies to LTSV data. This will be the labels of boolean fields.
+	BoolFieldLabels []string
+	// TagLabels only applies to LTSV data. This will be the labels of tags.
+	TagLabels []string
+	// DuplicatePointsModifierMethod only applies to LTSV data.
+	// Must be one of add_uniq_tag, increment_time, no_op.
+	// This will be used to modify duplicated points.
+	// For detail, please see https://docs.influxdata.com/influxdb/v0.10/troubleshooting/frequently_encountered_issues/#writing-duplicate-points
+	DuplicatePointsModifierMethod string
+	// DuplicatePointsModifierUniqTag only applies to LTSV data.
+	// When DuplicatePointsModifierMethod is "add_uniq_tag", this will be the label
+	// of the tag to be added to ensure uniqueness of points.
+	DuplicatePointsModifierUniqTag string
 
 	// DefaultTags are the default tags that will be added to all parsed metrics.
 	DefaultTags map[string]string
@@ -68,6 +98,20 @@ func NewParser(config *Config) (Parser, error) {
 	case "graphite":
 		parser, err = NewGraphiteParser(config.Separator,
 			config.Templates, config.DefaultTags)
+	case "ltsv":
+		parser, err = NewLTSVParser(
+			config.MetricName,
+			config.TimeLabel,
+			config.TimeFormat,
+			config.StrFieldLabels,
+			config.IntFieldLabels,
+			config.FloatFieldLabels,
+			config.BoolFieldLabels,
+			config.TagLabels,
+			config.DuplicatePointsModifierMethod,
+			config.DuplicatePointsModifierUniqTag,
+			config.DefaultTags,
+		)
 	default:
 		err = fmt.Errorf("Invalid data format: %s", config.DataFormat)
 	}
@@ -97,4 +141,33 @@ func NewGraphiteParser(
 	defaultTags map[string]string,
 ) (Parser, error) {
 	return graphite.NewGraphiteParser(separator, templates, defaultTags)
+}
+
+func NewLTSVParser(
+	metricName string,
+	timeLabel string,
+	timeFormat string,
+	strFieldLabels []string,
+	intFieldLabels []string,
+	floatFieldLabels []string,
+	boolFieldLabels []string,
+	tagLabels []string,
+	duplicatePointsModifierMethod string,
+	duplicatePointsModifierUniqTag string,
+	defaultTags map[string]string,
+) (Parser, error) {
+	parser := &ltsv.LTSVParser{
+		MetricName:                     metricName,
+		TimeLabel:                      timeLabel,
+		TimeFormat:                     timeFormat,
+		StrFieldLabels:                 strFieldLabels,
+		IntFieldLabels:                 intFieldLabels,
+		FloatFieldLabels:               floatFieldLabels,
+		BoolFieldLabels:                boolFieldLabels,
+		TagLabels:                      tagLabels,
+		DuplicatePointsModifierMethod:  duplicatePointsModifierMethod,
+		DuplicatePointsModifierUniqTag: duplicatePointsModifierUniqTag,
+		DefaultTags:                    defaultTags,
+	}
+	return parser, nil
 }
